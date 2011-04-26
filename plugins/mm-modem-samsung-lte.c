28452e9bb80f04c0c5bf22d62be02ee23be51466
Marius B. Kotsbak <marius@kotsbak.com>
2011-04-27 01:47:03 +0200

    Try to get AT+CGREG sent before AT+CFUN.

--- plugins/mm-modem-samsung-lte.c
+++ plugins/mm-modem-samsung-lte.c
@@ -381,17 +381,15 @@ get_property (GObject *object, guint prop_id,
     switch (prop_id) {
     /* This modem does not support "ATZ" command while in CFUN=0 mode */
     case MM_GENERIC_GSM_PROP_INIT_CMD:
-        g_value_set_string (value, "E0");
+        g_value_set_string (value, "+CGREG=2");
         break;
 
-        /*
     case MM_GENERIC_GSM_PROP_INIT_CMD_OPTIONAL:
         g_value_set_string (value, "+CFUN=5");
         break;
-        */
 
     case MM_GENERIC_GSM_PROP_POWER_UP_CMD:
-        g_value_set_string (value, "+CFUN=5");
+        g_value_set_string (value, "E0");
         break;
 
     default:
@@ -423,11 +421,9 @@ mm_modem_samsung_lte_class_init (MMModemSamsungLteClass *klass)
     g_object_class_override_property (object_class,
                                       MM_GENERIC_GSM_PROP_INIT_CMD,
                                       MM_GENERIC_GSM_INIT_CMD);
-    /*
     g_object_class_override_property (object_class,
                                       MM_GENERIC_GSM_PROP_INIT_CMD_OPTIONAL,
                                       MM_GENERIC_GSM_INIT_CMD_OPTIONAL);
-    */
     g_object_class_override_property (object_class,
                                       MM_GENERIC_GSM_PROP_POWER_UP_CMD,
                                       MM_GENERIC_GSM_POWER_UP_CMD);
